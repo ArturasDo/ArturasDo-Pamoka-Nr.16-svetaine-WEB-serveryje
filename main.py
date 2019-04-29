718b6641bb9fd302f7d8c5acedfc4ff8023685d5
ArturasDo <48279198+ArturasDo@users.noreply.github.com>
2019-04-29 23:37:16 +0300

    be bootstrapo

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -8,14 +8,25 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/about-me")
+@app.route("/about-me", methods=["GET"])
 def about():
     return render_template("about.html")
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "GET":
+        return render_template("contact.html")
+    elif request.method == "POST":
+        contact_name = request.form.get("contact-name")
+        contact_email = request.form.get("contact-email")
+        contact_message = request.form.get("contact-message")
+
+        print(contact_name)
+        print(contact_email)
+        print(contact_message)
+
+        return render_template("success.html")
 
 
 if __name__ == '__main__':
