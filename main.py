368fdaa3c1dac04a8eae09b49956ca537a118f5d
ArturasDo <48279198+ArturasDo@users.noreply.github.com>
2019-04-30 22:54:24 +0300

    dirbantis

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def index():
     return render_template("index.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form.get('username')
+        response = make_response(render_template ("success.html", username=username))
+        response.set_cookie("username", "test")
+        return render_template("success.html")
+    elif request.method == "GET":
+        return render_template("login.html")
 
 
 @app.route("/about-me", methods=["GET"])
